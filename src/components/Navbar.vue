c703077935026da0bc883db1fd64a15cd2e865e0
Toni Kolarić <toni14nexe@gmail.com>
2023-01-12 22:38:46 +0100

    Fixed mobile navbar automatically closing

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,7 +9,10 @@
 
         <div>
           <div class="d-flex justify-content-end">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" 
+              data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+              aria-expanded="false"
+            >
               <i class="fa-solid fa-list-ul fa-bars text-white"></i>
             </button>
           </div>
@@ -18,11 +21,19 @@
             <div class="collapse navbar-collapse my-2 my-lg-0" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item mr-sm-2">
-                  <span class="a d-flex justify-content-center" @click="$emit('priceList')" :class="{active: currentComponent == 'PriceList'}" href="#">Price List</span>
+                  <span class="a d-flex justify-content-center" 
+                    data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" 
+                    @click="$emit('priceList')" :class="{active: currentComponent == 'PriceList'}" href="#"
+                  >Price List
+                  </span>
                 </li>
                 <div class="vl"></div>
                 <li class="nav-item mr-sm-2">
-                  <span class="a d-flex justify-content-center" @click="$emit('login')" :class="{active: currentComponent == 'LogIn'}" href="#">LogIn</span>
+                  <span 
+                    data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" 
+                    class="a d-flex justify-content-center" @click="$emit('login')" :class="{active: currentComponent == 'LogIn'}" href="#"
+                  >LogIn
+                  </span>
                 </li>
               </ul>
             </div>
@@ -32,19 +43,32 @@
             <div class="collapse navbar-collapse my-2 my-lg-0" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li v-if="role == 'table'" class="nav-item mr-sm-2">
-                  <span class="a d-flex justify-content-center" @click="$emit('priceList')" :class="{active: currentComponent == 'PriceList'}" href="#">Order</span>
+                  <span 
+                    data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" 
+                    class="a d-flex justify-content-center" @click="$emit('priceList')" :class="{active: currentComponent == 'PriceList'}" href="#"
+                  >Order
+                  </span>
                 </li>
                 <div v-if="role == 'table'" class="vl"></div>
                 <li v-if="role == 'table'" class="nav-item mr-sm-2">
-                  <span class="a d-flex justify-content-center" @click="$emit('cart')" :class="{active: currentComponent == 'Cart'}" href="#">Cart</span>
+                  <span data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" 
+                    class="a d-flex justify-content-center" @click="$emit('cart')" :class="{active: currentComponent == 'Cart'}" href="#">Cart</span>
                 </li>
                 <div v-if="role == 'table' && bill" class="vl"></div>
                 <li v-if="role == 'table' && bill" class="nav-item mr-sm-2">
-                  <span class="a d-flex justify-content-center" @click="$emit('bill')" :class="{active: currentComponent == 'Bill'}" href="#">Bill</span>
+                  <span 
+                    data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" 
+                    class="a d-flex justify-content-center" @click="$emit('bill')" :class="{active: currentComponent == 'Bill'}" href="#"
+                  >Bill
+                  </span>
                 </li>
                 <div v-if="role == 'table'" class="vl"></div>
                 <li class="nav-item mr-sm-2">
-                  <span class="a d-flex justify-content-center" @click="$emit('logout')" href="#">LogOut</span>
+                  <span 
+                    data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" 
+                    class="a d-flex justify-content-center" @click="$emit('logout')" href="#"
+                  >LogOut
+                  </span>
                 </li>
               </ul>
             </div>
